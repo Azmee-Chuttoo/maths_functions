3b36e9672cc621ee3c5de04e9b2b4c0217bdc1ae
Azmee Muhammad Chuttoo <P14134336@GWHXX474LIN013W.lec-admin.dmu.ac.uk>
2016-02-09 19:35:48 +0000

    Multiply instruction

---
+++ multiply.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+ 
+int main()
+{
+   int a, b, c;
+ 
+   printf("Enter two numbers to multiply\n");
+   scanf("%d%d",&a,&b);
+ 
+   c = a * b;
+ 
+   printf("Sum of entered numbers = %d\n",c);
+ 
+   return 0;
+}
