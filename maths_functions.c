8bdf772dbba23b4c8c858cbbe7898a5937dcc085
Azmee Muhammad Chuttoo <P14134336@GWHXX653LIN005W.lec-admin.dmu.ac.uk>
2016-02-10 13:18:04 +0000

    All maths function together

---
+++ maths_functions.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+ 
+int main()
+{
+   int a, b, c;
+ 
+   printf("Enter two numbers to Addition\n");
+   scanf("%d%d",&a,&b);
+ 
+   c = a + b;
+ 
+   printf("Sum of entered numbers = %d\n",c);
+ 
+
+
+   printf("Enter two numbers to subtraction\n");
+   scanf("%d%d",&a,&b);
+ 
+   c = a - b;
+ 
+   printf("Sum of entered numbers = %d\n",c);
+ 
+
+
+ 
+   printf("Enter two numbers to multiply\n");
+   scanf("%d%d",&a,&b);
+ 
+   c = a * b;
+ 
+   printf("Sum of entered numbers = %d\n",c);
+ 
+ 
+
+
+   printf("Enter two numbers to division\n");
+   scanf("%d%d",&a,&b);
+ 
+   c = a % b;
+ 
+   printf("Sum of entered numbers = %d\n",c);
+ 
+   return 0;
+}
+
